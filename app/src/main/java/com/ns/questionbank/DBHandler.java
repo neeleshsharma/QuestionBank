30bade1659742f49231d9e6e23211741a047ff3c
neeleshsharma <neelesh_sharma2002@yahoo.com>
2016-08-10 10:36:06 +0530

    Unstable Fourth Commit: 8 Aug

--- app/src/main/java/com/ns/questionbank/DBHandler.java
+++ app/src/main/java/com/ns/questionbank/DBHandler.java
@@ -3,8 +3,11 @@ package com.ns.questionbank;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +18,7 @@ import java.util.List;
 public class DBHandler extends SQLiteOpenHelper
 
 {
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     private static final String DATABASE_NAME = "biologyQuestions";
     private static final String TABLE_QUESTION_BANK = "questionbank";
     private static final String KEY_QUESTION_NUMBER = "question_number";
@@ -25,6 +28,10 @@ public class DBHandler extends SQLiteOpenHelper
     private static final String KEY_OPTION_TWO = "option_two";
     private static final String KEY_OPTION_THREE = "option_three";
     private static final String KEY_OPTION_FOUR = "option_four";
+
+    private static final String TABLE_USER_ANSWER = "useranswer";
+    private static final String KEY_USER_QUESTION_NUMBER = "question_number";
+    private static final String KEY_USER_ANSWER = "user_answer";
 
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -41,6 +48,15 @@ public class DBHandler extends SQLiteOpenHelper
                 + KEY_OPTION_FOUR + " TEXT "
                 + ")";
         db.execSQL(CREATE_QUESTION_BANK_TABLE);
+
+        String CREATE_USER_ANSWER_TABLE = " CREATE TABLE "+ TABLE_USER_ANSWER+ "("
+                +KEY_USER_QUESTION_NUMBER + " INTEGER PRIMARY KEY , "
+                +KEY_USER_ANSWER + " INTEGER"
+                + ")";
+        try
+        {
+            db.execSQL(CREATE_USER_ANSWER_TABLE);
+        }catch (SQLException e) { Log.d("errorininsert",null,null);e.printStackTrace();};
     }
 
     @Override
@@ -77,7 +93,18 @@ public class DBHandler extends SQLiteOpenHelper
     {
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor c = db.rawQuery("select * from " + TABLE_QUESTION_BANK,null);
+       // c.close();
         return c.getCount();
+
+    }
+
+    public int getAnswerCount()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c1 = db.rawQuery("select * from " + TABLE_USER_ANSWER,null);
+        // c.close();
+        return c1.getCount();
+
     }
 
     public List<QuestionBank> getAllQuestion() {
@@ -99,7 +126,10 @@ public class DBHandler extends SQLiteOpenHelper
 
             } while (cursor.moveToNext());
         }
+        //cursor.close();
+        db.close();
         return questionBankList;
+
 
     }
 
@@ -115,9 +145,71 @@ public class DBHandler extends SQLiteOpenHelper
                 Integer.parseInt(cursor.getString(0)),
                 Integer.parseInt(cursor.getString(1)),
                 cursor.getString(2), cursor.getString(3),cursor.getString(4),cursor.getString(5),cursor.getString(6));
-
+        //cursor.close();
+        db.close();
         return questionbank;
     }
+    public void addUpdateYourAnswer(UserAnswer userAnswer)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        ContentValues values1 = new ContentValues();
+        values.put(KEY_USER_ANSWER,userAnswer.getUserAnswer());
+        values1.put(KEY_USER_ANSWER,userAnswer.getUserAnswer());
+        values.put(KEY_USER_QUESTION_NUMBER, userAnswer.getQuestionNumber());
+        Cursor cr = db.rawQuery("SELECT * FROM "+ TABLE_USER_ANSWER +" WHERE question_number = " + userAnswer.getQuestionNumber(),null);
+        if (cr == null) db.insert(TABLE_USER_ANSWER, null, values);
+        //else  db.update(TABLE_USER_ANSWER,values1,null);
+        else db.execSQL("Update "+ TABLE_USER_ANSWER +" SET "+ KEY_USER_ANSWER+ " = "+userAnswer.getUserAnswer() +" WHERE "
+        + KEY_QUESTION_NUMBER +" = "+ userAnswer.getQuestionNumber()
+        );
+        db.close();
+    }
 
+    public List<UserAnswer> getUserAnswer()
+    {
+        List<UserAnswer> userAnswerList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_USER_ANSWER;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+        if (cursor.moveToFirst())
+        {
+            do
+            {
+                  UserAnswer userAnswer = new UserAnswer();
+                    userAnswer.setQuestionNumber(Integer.parseInt(cursor.getString(0)));
+                    userAnswer.setYourAnswer(Integer.parseInt(cursor.getString(1)));
+                }while (cursor.moveToNext());
+
+        }
+        //cursor.close();
+        return userAnswerList;
+
+    }
+
+    public int isCorrectAnswer(int questionNumber)
+    {
+        int i=0;
+        int j=0;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selectquery = "SELECT " + KEY_CORRECT_ANSWER +" FROM " +TABLE_QUESTION_BANK
+                +" WHERE question_number = "  +questionNumber;
+        Cursor cursor = db.rawQuery(selectquery,null);
+      //  i=(int)Integer.parseInt(cursor.getString(0));
+        if (cursor !=null) {
+            if (cursor.moveToFirst()) i = cursor.getInt(0);
+        }
+
+        String selectquery1 = "SELECT " + KEY_USER_ANSWER +" FROM " +TABLE_USER_ANSWER
+                +" WHERE question_number = "  +questionNumber;
+        Cursor cursor1 = db.rawQuery(selectquery1,null);
+        //j=Integer.parseInt(cursor.getString(0));
+        if (cursor !=null) {
+            if (cursor1.moveToFirst()) j= cursor1.getInt(0);
+        }
+        if ( i == j) return 1 ;else return 0;
+        //Select correct_answer from questionbank where question_number = questionNumber
+    }
 
 }
